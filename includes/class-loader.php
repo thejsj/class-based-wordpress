928dbc29273e54bfc87c65712c0dbe4bd8d46c6a
thejsj <jorge.silva@thejsj.com>
2014-08-28 08:13:58 -0400

    Changing classes to models

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -2,7 +2,7 @@
 
 	function __autoload($class_name) {
 		if (!class_exists($class_name)) {
-			$class_filename = trailingslashit( get_stylesheet_directory()) . 'classes/'. camel2dashed($class_name) . '.class.php';
+			$class_filename = trailingslashit( get_stylesheet_directory()) . 'models/'. camel2dashed($class_name) . '.class.php';
 			$view_filename = trailingslashit( get_stylesheet_directory()) . 'views/'. camel2dashed($class_name) . '.class.php';
 			if (file_exists($class_filename)) {
 				include_once($class_filename);
